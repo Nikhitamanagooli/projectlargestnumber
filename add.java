999cc54f1f3d5383612adc6eefe52806cf746d9b
Nikhitamanagooli <01fe22bca033@gmail.com>
2024-01-28 21:43:24 +0530

    code changes related to addition function

---
+++ add.java
@@ -0,0 +1,9 @@
+class add {
+    public static void main(String[] args) {
+        int a=2;
+        int b=4;
+        int result;
+        result=a+b;
+       System.out.println("addition:"+ result); 
+    }
+}
